5c60961382c18ee6b7deb190dfd5fe53423162fb
valentinolie24 <valenlie2409@gmail.com>
2022-10-11 17:08:27 +0700

    Update Project

--- app/src/main/java/com/si5a/myapplication/MainActivity.java
+++ app/src/main/java/com/si5a/myapplication/MainActivity.java
@@ -6,9 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     Button btnPindah;
+    EditText etNama;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +20,22 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Layout A");
 
         btnPindah = findViewById(R.id.btn_pindah);
+        etNama = findViewById(R.id.et_nama);
+
         btnPindah.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent pindah = new Intent(MainActivity.this, SecondActivity.class);
-                startActivity(pindah);
+                String nama = etNama.getText().toString();
+
+                if (nama.trim().equals("")){
+                    etNama.setError("Nama Harus Diisi");
+                }
+                else {
+                    Intent pindah = new Intent(MainActivity.this, SecondActivity.class);
+                    pindah.putExtra("xNama", nama);
+                    startActivity(pindah);
+                }
+
             }
         });
     }
